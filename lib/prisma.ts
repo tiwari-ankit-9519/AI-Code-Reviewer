c31365a317e1113678e822fc31197134c47a938b
tiwari-ankit-9519 <tiwari.ankit3105@gmail.com>
2025-11-28 01:18:54 +0530

    Fixed Type Error in exports6

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,10 +1,22 @@
 import { PrismaClient } from "@prisma/client";
+import { PrismaPg } from "@prisma/adapter-pg";
+import pg from "pg";
 
 const globalForPrisma = globalThis as unknown as {
   prisma: PrismaClient | undefined;
 };
 
-export const prisma = globalForPrisma.prisma ?? new PrismaClient();
+const adapter = new PrismaPg(
+  new pg.Pool({
+    connectionString: process.env.DATABASE_URL,
+  })
+);
+
+export const prisma =
+  globalForPrisma.prisma ??
+  new PrismaClient({
+    adapter,
+  });
 
 if (process.env.NODE_ENV !== "production") {
   globalForPrisma.prisma = prisma;
