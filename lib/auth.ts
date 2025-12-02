e01255447132f85b5eec4e7208ee851ed4e74917
tiwari-ankit-9519 <tiwari.ankit3105@gmail.com>
2025-12-02 13:21:38 +0530

    Fixed authURL

--- lib/auth.ts
+++ lib/auth.ts
@@ -170,15 +170,8 @@ export const { handlers, auth, signIn, signOut } = NextAuth({
       return session;
     },
 
-    async redirect({ url, baseUrl }) {
-      log("INFO", "Redirect callback", { url, baseUrl });
-
-      if (url.startsWith("/")) {
-        return `${baseUrl}${url}`;
-      } else if (new URL(url).origin === baseUrl) {
-        return url;
-      }
-      return baseUrl;
+    async redirect({ url }) {
+      return url;
     },
   },
 
